26b89d04f02f2acef8edeef07dcc35e9ecce2eb0
Ramadhani-Yassin <yasynramah@gmail.com>
2025-03-28 11:29:23 +0300

    UNIDOCS

--- UNIDOCS Front End/src/app/components/header/header.component.ts
+++ UNIDOCS Front End/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,12 +8,42 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   menuOpen = false;
   dropdownOpen = false;
+  isMobile = window.innerWidth <= 768;
+
+  @HostListener('window:resize')
+  onResize() {
+    this.isMobile = window.innerWidth <= 768;
+    if (!this.isMobile) {
+      this.menuOpen = false;
+    }
+  }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+    if (!this.menuOpen) {
+      this.dropdownOpen = false;
+    }
   }
 
-  toggleDropdown() {
+  toggleDropdown(event: Event) {
+    event.preventDefault(); // Prevent default to avoid unwanted behavior
     this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  showSubmenu() {
+    this.dropdownOpen = true;
+  }
+
+  hideSubmenu() {
+    this.dropdownOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeMenus(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('nav') || target.classList.contains('dashboard-link')) {
+      return;
+    }
+    this.dropdownOpen = false;
   }
 }
