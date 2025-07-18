47797e43ff9cdf0b8fa55a8855be11d93b870f66
Ramadhani-Yassin <yasynramah@gmail.com>
2025-07-18 12:57:43 +0300

    try cursor

--- Front End/src/environments/environment.ts
+++ Front End/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://10.18.68.248:8088' // Your existing API URL
+  apiUrl: 'http://10.227.104.248:8088' // Your existing API URL
   // Add this if you want to use apiBaseUrl instead:
-  // apiBaseUrl: 'http://10.18.68.248:8088'
+  // apiBaseUrl: 'http://10.227.104.248:8088'
 };
